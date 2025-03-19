ccb551d0215979f2a126668e1f7ecc36d0abc87b
Shakiqurrahman <rahmanshakiqur@gmail.com>
2025-03-20 02:19:24 +0600

    bug fixed

--- src/providers/StoreProvider.tsx
+++ src/providers/StoreProvider.tsx
@@ -1,7 +1,7 @@
 import { AppStore, makeStore } from "@/redux/store";
 import { ReactNode, useRef } from "react";
 import { Provider } from "react-redux";
-import { persistStore } from "redux-persist";
+// import { persistStore } from "redux-persist";
 
 export default function StoreProvider({ children }: { children: ReactNode }) {
   const storeRef = useRef<AppStore>(undefined);
@@ -10,7 +10,7 @@ export default function StoreProvider({ children }: { children: ReactNode }) {
     storeRef.current = makeStore();
   }
 
-  const persistedStore = persistStore(storeRef.current);
+  // const persistedStore = persistStore(storeRef.current);
   return (
     <Provider store={storeRef.current}>
       {/* <PersistGate loading={<Loading />} persistor={persistedStore}> */}
